21368b6ce3fcfed4ca7f2a2279d655b202eff7a2
andrew-canafe <andrew.g.canafe@gmail.com>
2021-08-11 09:30:55 -0700

    Fix report view component

--- TeaCoffeeVendingMachineF/src/app/component/report-view/report-view.component.ts
+++ TeaCoffeeVendingMachineF/src/app/component/report-view/report-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SaleService } from 'src/app/service/sale-service/sale-service.service';
 @Component({
-  selector: 'app-report',
+  selector: 'app-report-view',
   templateUrl: './report-view.component.html',
   styleUrls: ['./report-view.component.css']
 })
